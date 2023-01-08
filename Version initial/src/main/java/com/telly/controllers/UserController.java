fc933552498a1a2b6a80f38805144e64efdf5cc8
Sid Ahmed Boudaoud <57918243+Sidou10major@users.noreply.github.com>
2023-01-08 21:08:31 +0100

    Création d'un utilisateur

--- Version initial/src/main/java/com/telly/controllers/UserController.java
+++ Version initial/src/main/java/com/telly/controllers/UserController.java
@@ -1,8 +1,25 @@
 package com.telly.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.telly.dao.FormValidationGroup;
+import com.telly.dao.Reserve;
+import com.telly.dao.User;
+import com.telly.service.ReserveService;
+import com.telly.service.UserService;
+import java.security.Principal;
+@Controller
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ReserveService reserveService;
     @RequestMapping("/login")
     public String showLogin() {
         return "login";
@@ -10,5 +27,28 @@ public class UserController {
     @RequestMapping("/loggedout")
     public String showLogout() {
         return "loggedout";
+    }
+    @RequestMapping("/createaccount")
+    public String createAccount(Model model, Principal principal) {
+
+        model.addAttribute("user", new User());
+
+        return "createaccount";
+    }
+
+    @RequestMapping(value = "/createuser", method = RequestMethod.POST)
+    public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "createaccount";
+        }
+
+        user.setAuthority("ROLE_USER");
+        user.setEnabled(true);
+
+        userService.create(user);
+
+        return "home";
+
     }
 }
